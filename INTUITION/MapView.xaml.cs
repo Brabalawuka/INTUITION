32988940702c4cb3b9fe635eb134b3793d59ef99
Lyu Zizheng <lyuz0001@e.ntu.edu.sg>
2018-10-13 15:41:21 +0800

    aa

--- INTUITION/MapView.xaml.cs
+++ INTUITION/MapView.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +26,18 @@ namespace INTUITION
         public MapView()
         {
             this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Specify a known location.
+            BasicGeoposition cityPosition = new BasicGeoposition() { Latitude = 1.348, Longitude = 103.6827 };
+            Geopoint cityCenter = new Geopoint(cityPosition);
+
+            // Set the map location.
+            MapControl1.Center = cityCenter;
+            MapControl1.ZoomLevel = 15.4;
+            MapControl1.LandmarksVisible = true;
         }
     }
 }
